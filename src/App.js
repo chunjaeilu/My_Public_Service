9fda57f3cc2f9bc5595bbf100d180cddf0a1fe93
chunjaeilu <chunjaeilu@naver.com>
2023-02-03 16:39:54 +0900

    프로젝트 확정

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
 import "./App.css";
-import { useState, useEffect } from "react";
-import Items from "./components/Items";
+// import { useState, useEffect } from "react";
+// import Items from "./components/Items";
 
 function App() {
-  const API_KEY = `2rRLJsTlQ8FptpGLqMHFM0BXJA5f4jBxNUlKeLQRYwOI4QpobCOT2pVOw9ddrq273ybB5XcgQjf4J8GGVAhfrg%3D%3D`;
+  const API_KEY = "063b6a61-eb28-4e98-a95d-d6796590353f";
 
-  const API_URL = `http://apis.data.go.kr/6260000/BusanPetAnimalInfoService/getPetAnimalInfo?serviceKey=${API_KEY}&numOfRows=10&pageNo=5&resultType=json`;
+  const API_URL = `http://api.kcisa.kr/openapi/service/rest/convergence/conver6?serviceKey=${API_KEY}`;
 
-  const [animals, setAnimals] = useState([]);
+  // const [infos, setInfos] = useState([]);
 
-  const getAnimalData = () => {
-    fetch(API_URL)
+  const getArthallData = () => {
+    fetch(API_URL, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
       .then((res) => res.json())
       .then((data) => {
-        const items = data.getPetAnimalInfo.body.items.item;
+        // const items = data.response.body.items.item;
         // 데이터를 state에 저장
-        setAnimals(items);
+        console.log(data.response.body.items.item);
+        // setInfos(items);
       })
       .catch((e) => {
         console.log(e);
       });
   };
-  useEffect(() => {
-    getAnimalData();
-  }, []);
-  console.log(animals);
+  // useEffect(() => {
+  //   getAnimalData();
+  // }, []);
+  getArthallData();
   return (
     <div className="wrap">
       <h1>RESCUE119</h1>
       <h3>부산시 동물구조 현황</h3>
-      {animals.length !== 0 ? <Items animals={animals} /> : <p>로딩중...</p>}
+      {/* {infos.length !== 0 ? <Items infos={infos} /> : <p>로딩중...</p>} */}
     </div>
   );
 }
