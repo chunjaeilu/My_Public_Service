1f009a709dd39bed80d8fa50d908702c107f40bb
chunjaeilu <chunjaeilu@naver.com>
2023-02-06 13:32:07 +0900

    반복문

--- README.md
+++ README.md
@@ -10,24 +10,27 @@
 ## ISSUES
 
 ### 방대한 DB 처리
-> 총 9919개의 방대한 DB를 한번에 불러오면 트래픽 초과 오류 발생
+> 총 9919개의 방대한 DB를 한번에 불러오면 트래픽 초과 오류 빈번히 발생
 >
-> 한 페이지에서 불러올 수 있는 데이터는 최대 100개
+> 일반적인 `fetch`를 실행했을 때 api url을 읽어오기 전에 처리문의 `filter()`함수가 실행되어 400error 발생
 
-#### solution :  숫자 배열 생성 및 활용
-- 1~100 까지 숫자를 인자로 갖는 배열 생성
-- `fetch` url에 들어가는 page 변수값으로 활용
+#### solution : 비동기 처리, 이중반복문 활용
+- 1~100 까지 숫자를 인자로 갖는 배열 생성 >> 이중반복문 변수값으로 활용
+- `fetch()`를 `async`, `await` 이용해 비동기 처리
   ```javascript
   const numArr100 = Array.from({ length: 100 }, (_, index) => index + 1);
-  ```
-- `numArr100` 변수를 인자로 하는 반복문 안에 `fetch()` 포함시킴
-  ```javascript
-  for (const page of numArr100) {
-    API_URL = `${URL}${api_list[0]}?&page=${page}&perPage=100&serviceKey=${API_KEY}`;
-    const res = await fetch(API_URL);
-    const data = await res.json();
-    ...
-  }
+  const [search, setSearch] = useState([]);
+  // 조건검색 함수
+  const getSearchData = async (major) => {
+    for (const page of numArr100) {
+      API_URL = `${URL}${api_list[0]}?&page=${page}&perPage=100&serviceKey=${API_KEY}`;
+      const res = await fetch(API_URL);
+      const data = await res.json();
+      const filteredSearch = data.data.filter((item) => item.소관기관명 === major);
+      setSearch([...search, ...filteredSearch]);
+    }
+  };
+  getSearchData(major);
   ```
 
 ### 검색 결과 전역변수에 기억시키기
--- src/App.js
+++ src/App.js
@@ -1,85 +1,69 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 import Items from "./components/Items";
 
 function App() {
   const API_KEY =
     "2rRLJsTlQ8FptpGLqMHFM0BXJA5f4jBxNUlKeLQRYwOI4QpobCOT2pVOw9ddrq273ybB5XcgQjf4J8GGVAhfrg==";
-
   const URL = "https://api.odcloud.kr/api/gov24/v1/";
-
   const api_list = ["serviceList", "serviceDetail", "supportConditions"];
-
   let API_URL = `${URL}${api_list[0]}?&page=1&perPage=10&serviceKey=${API_KEY}`;
+
+  // 서비스리스트
   const [serviceList, setServiceList] = useState([]);
-  // pages Arr [1~100]
-  const numArr100 = Array.from({ length: 100 }, (_, index) => index + 1);
+  // 검색결과 반영
+  const [search, setSearch] = useState([]);
+  // 검색어
+  const [searchInput, setSearchInput] = useState("");
+
+  //numArr [1~10]
+  const numArr10 = Array.from({ length: 10 }, (_, index) => index + 1);
 
   // 서비스목록 불러오기
-  const getServiceListData = () => {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        const items = data.data;
 
-        // 데이터를 state에 저장
-        setServiceList(items);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
+  const getServiceListData = async () => {
+    for (const page of numArr10) {
+      API_URL = `${URL}${api_list[0]}?&page=${page}&perPage=1000&serviceKey=${API_KEY}`;
+      const response = await axios.get(API_URL);
+      console.log(response.data.data);
+      const data = response.data.data;
+      const copyArr = serviceList;
+      setServiceList([...copyArr, ...data]);
+    }
   };
 
-  const [search, setSearch] = useState([]);
   // 조건검색 함수
-
-  const getSearchData = async (major) => {
-    for (const page of numArr100) {
-      API_URL = `${URL}${api_list[0]}?&page=${page}&perPage=100&serviceKey=${API_KEY}`;
+  const getSearchData = async (searchInput) => {
+    for (const page of numArr10) {
+      API_URL = `${URL}${api_list[0]}?&page=${page}&perPage=1000&serviceKey=${API_KEY}`;
       const res = await fetch(API_URL);
       const data = await res.json();
       const filteredSearch = data.data.filter(
-        (item) => item.소관기관명 === major
+        (item) => item.소관기관명 === searchInput
       );
-      setSearch([...search, ...filteredSearch]);
+      setSearch(search.concat(filteredSearch));
+
+      console.log(search);
     }
   };
-  console.log(search);
 
-  // const getSearchData = (major) => {
-  //   numArr100.map((page) => {
-  //     API_URL = `${URL}${api_list[0]}?&page=${page}&perPage=100&serviceKey=${API_KEY}`;
-  //     fetch(API_URL)
-  //       .then((res) => res.json())
-  //       .then((data) => {
-  //         let searchItems = data.data;
-
-  //         searchItems.map((item) => {
-  //           return item.소관기관명 == major
-  //             ? setSearch([...search, item])
-  //             : setSearch([...search]);
-  //         });
-  //         const filteredSearch = searchItems.filter(
-  //           (item) => item.소관기관명 === major
-  //         );
-  //         setSearch([...search, filteredSearch]);
-  //       })
-  //       .catch((e) => {
-  //         console.log(e);
-  //       });
-  //   });
-  // };
+  // onChange 함수
+  const onChange = (e) => {
+    const inputValue = e.target.value;
+    setSearchInput(inputValue);
+  };
 
   useEffect(() => {
     getServiceListData();
-    getSearchData("해양수산부");
   }, []);
 
   return (
     <div className="wrap">
       <h1>나의 공공서비스</h1>
       <h3>공공서비스 목록</h3>
+      <input type="text" placeholder="검색어 입력" onChange={onChange} />
+      <button onClick={() => getSearchData(searchInput)}>검색</button>
       {serviceList.length !== 0 ? (
         <Items serviceList={serviceList} />
       ) : (
