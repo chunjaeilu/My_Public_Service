f77074a3ffe05ba870e77fad848571430d8de2c8
chunjaeilu <chunjaeilu@naver.com>
2023-02-06 17:47:28 +0900

    검색기능 작동 확인

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     "2rRLJsTlQ8FptpGLqMHFM0BXJA5f4jBxNUlKeLQRYwOI4QpobCOT2pVOw9ddrq273ybB5XcgQjf4J8GGVAhfrg==";
   const URL = "https://api.odcloud.kr/api/gov24/v1/";
   const api_list = ["serviceList", "serviceDetail", "supportConditions"];
-  let API_URL = `${URL}${api_list[0]}?&page=1&perPage=10&serviceKey=${API_KEY}`;
+  let API_URL = `${URL}${api_list[0]}?&page=1&perPage=1000&serviceKey=${API_KEY}`;
 
   // 서비스리스트
   const [serviceList, setServiceList] = useState([]);
@@ -19,35 +19,57 @@ function App() {
   // show
   const [showList, setShowList] = useState([]);
 
-  //numArr [1~10]
-  // const numArr10 = Array.from({ length: 10 }, (_, index) => index + 1);
-
+  // 소속기관Arr
+  const majorArr = [
+    "교육부",
+    "국세청",
+    "금융위원회",
+    "해양수산부",
+    "관세청",
+    "통계청",
+    "통일부",
+    "법무부",
+    "대검찰청",
+    "국방부",
+    "행정안전부",
+    "경찰청",
+    "보건복지부",
+    "질병관리청",
+    "기상청",
+    "문화체육관광부",
+    "여성가족부",
+    "농촌진흥청",
+    "산림청",
+    "중소벤처기업부",
+    "특허청",
+    "산업통상자원부",
+    "식품의약품안전처",
+    "환경부",
+    "고용노동부",
+    "농림축산식품부",
+    "문화재청",
+    "국토교통부",
+    "방위사업청",
+    "과학기술정보통신부",
+  ];
   // 서비스목록 불러오기
-
   const getServiceListData = async () => {
-    API_URL = `${URL}${api_list[0]}?&page=1&perPage=1000&serviceKey=${API_KEY}`;
     const response = await axios.get(API_URL);
     console.log(response.data.data);
-    const data = response.data.data;
-    setServiceList(serviceList.concat(data));
-  };
+    let data = response.data.data;
 
-  const getShowList = () => {
-    let newArr = [...serviceList];
-    newArr.sort((a, b) => b.조회수 - a.조회수);
-    const topList = newArr.slice(0, 10);
+    const major = data.map((e) => e.소관기관명);
+
+    const set = new Set(major);
+    console.log(set);
+    // serviceList 업데이트
+    setServiceList([...data]);
+
+    // showList 가공
+    data.sort((a, b) => b.조회수 - a.조회수);
+    const topList = data.slice(0, 10);
     setShowList([...topList]);
-  };
-
-  // 조건검색 함수
-  const getSearchData = (searchInput) => {
-    const filteredSearch = serviceList.filter(
-      (item) => item.소관기관명 === searchInput
-    );
-    setSearch(search.concat(filteredSearch));
-    setServiceList([...search]);
-
-    console.log(search);
+    console.log(topList);
   };
 
   // onChange 함수
@@ -56,13 +78,27 @@ function App() {
     setSearchInput(inputValue);
   };
 
+  // 조건검색 함수
+  const getSearchData = (searchInput) => {
+    if (searchInput.length === 0) {
+      getServiceListData();
+    } else {
+      let filteredSearch = serviceList.filter((item) =>
+        item.서비스명.includes(searchInput)
+      );
+      setSearch([...filteredSearch]);
+
+      filteredSearch.sort((a, b) => b.조회수 - a.조회수);
+
+      setShowList([...filteredSearch]);
+    }
+  };
+  console.log(search);
+
+  // 마운트시 api 호출
   useEffect(() => {
     getServiceListData();
   }, []);
-
-  useEffect(() => {
-    getShowList();
-  }, [serviceList]);
 
   return (
     <div className="wrap">
@@ -70,7 +106,7 @@ function App() {
       <h3>공공서비스 목록</h3>
       <input type="text" placeholder="검색어 입력" onChange={onChange} />
       <button onClick={() => getSearchData(searchInput)}>검색</button>
-      {serviceList.length !== 0 ? (
+      {showList.length !== 0 ? (
         <ShowItems showList={showList} />
       ) : (
         <p>로딩중...</p>
--- src/components/ShowItems.js
+++ src/components/ShowItems.js
@@ -5,11 +5,11 @@ export default function ShowItems({ showList }) {
         return (
           <div className="items" key={i}>
             <h3>{item.서비스명}</h3>
+            <p className="sm-right">조회수 : {item.조회수}</p>
             <p>
               소관기관 : {item.소관기관명} / {item.부서명}
             </p>
             <p>지원대상 : {item.지원대상}</p>
-            <p>지원내용 : {item.지원내용}</p>
           </div>
         );
       })}
